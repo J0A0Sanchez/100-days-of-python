2d181a12261c5efa54880a5642cb963d1d963938
Joao Sanchez <jvmsanchez0@gmail.com>
2025-11-14 16:49:05 +0000

    update

---
+++ day6_to_day10/reeborg2.py
@@ -0,0 +1,23 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+# ExerExercise: Hurdle 3
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+    
+# while at_goal() == False:
+#     if front_is_clear():
+#         move()
+#     elif wall_in_front():
+#         jump()
---
+++ day6_to_day10/reeborg3.py
@@ -0,0 +1,31 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+# ExerExercise: Hurdle 4
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     turn_left()
+#     c=0
+#     while wall_on_right() == True:
+#         move()
+#         c+=1
+        
+#     turn_right()
+#     move()
+#     turn_right()
+    
+#     while c > 0:
+#         move()
+#         c-=1
+        
+#     turn_left()
+
+    
+# while at_goal() == False:
+#     if front_is_clear():
+#         move()
+#     elif wall_in_front():
+#         jump()
