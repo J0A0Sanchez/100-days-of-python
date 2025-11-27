ae940dcaad25995305fea6b4b5bdb93b30f9d396
Joao Sanchez <jvmsanchez0@gmail.com>
2025-11-27 09:38:40 +0000

    update

--- day11_to_day15/prime_number_checker.py
+++ day11_to_day15/prime_number_checker.py
@@ -6,9 +6,11 @@ def is_prime(num):
     if num <= 1:
         return False
     
+    
     for i in range(2, num):
         if num % i == 0:
             return False
+        
     
     if num % 1 == 0 and num % num == 0:
         return True
---
+++ day11_to_day15/try_except.py
@@ -0,0 +1,11 @@
+#Try and excpet example:
+
+
+try:
+    age = int(input("Enter your age: "))
+except ValueError:
+    print("Invalid input! Please enter a numeric value for age.")
+    age = int(input("Enter your age: "))
+
+if age >= 18:
+    print(f"You can drive at age {age}")
